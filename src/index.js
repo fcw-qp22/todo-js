7d1e105a30c892f2be5490d736bdee1c80dfce6f
fcw-qp22 <82388239+fcw-qp22@users.noreply.github.com>
2022-03-23 17:35:55 +0900

    taskstextadd

--- src/index.js
+++ src/index.js
@@ -8,7 +8,16 @@ const onClickAdd = () => {
   //div生成
   const div = document.createElement("div");
   div.className = "list-row";
-  console.log(div);
+
+  //liタグを生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  // divタグの子要素に各要素を設定
+  div.appendChild(li);
+
+  //未完了のリストに追加
+  document.getElementById("incomplete-list").appendChild(div);
 };
 
 document
